6b76ef11375cef08ba006bcd12641a5860b88534
GOTO Satoru <satoru.goto@mariadb.com>
2018-10-13 21:58:45 +0900

    added MaxScale 2.3 binlogrouter master failover

--- provisioning/maxscale/files/start-slave2.sql
+++ provisioning/maxscale/files/start-slave2.sql
@@ -1,5 +1,4 @@
 select @@global.gtid_current_pos;
--- SET @@global.gtid_slave_pos='0-1-6';
 stop slave;
 CHANGE MASTER ':2' TO MASTER_HOST='192.168.2.12',MASTER_PORT=3306,MASTER_USER='maxuser',MASTER_PASSWORD='maxpwd',MASTER_USE_GTID=Slave_pos;
 start slave;
